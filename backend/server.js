72c6131e7f0c1a313e2f89f36654f08a11624fe3
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-01-15 19:36:01 +1100

    added: heart route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,14 @@ import express from 'express'
 const port = 5000;
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Server is healthy");
+})
+
+app.get("/heart", (req, res) => {
+  res.send("Server is healthy");
+})
+
 app.listen(port, () => {
   console.log(`server started at https://localhost:${port}`);
 });
