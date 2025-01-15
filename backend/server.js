9ec003fa9ed96f66a08e640ee6ec3a86b28962e4
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-01-15 19:58:47 +1100

    added: database connection

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { connectDB } from './config/db.js';
 
 const port = 5000;
 const app = express();
@@ -7,10 +8,15 @@ app.get("/", (req, res) => {
   res.send("Server is healthy");
 })
 
+app.get("/items", (req, res) => {
+  res.send("todo");
+})
+
 app.get("/heart", (req, res) => {
   res.send("Server is healthy");
 })
 
 app.listen(port, () => {
+  connectDB();
   console.log(`server started at https://localhost:${port}`);
 });
