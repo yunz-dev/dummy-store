bd8365f66fb68b56a2995eca7406a782b43b3e8d
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-01-16 10:45:36 +1100

    added: product POST endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,32 @@
 import express from 'express'
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 const port = 5000;
 const app = express();
+
+app.use(express.json()); //middle ware to accept json data in req.body
 
 app.get("/", (req, res) => {
   res.send("Server is healthy");
 })
 
-app.get("/items", (req, res) => {
-  res.send("todo");
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.image || !product.price) {
+    return res.status(400).json({success:false, message: "Not all fields were provided"});
+  }
+
+  const newProduct = new Product(product)
+
+  try {
+    await newProduct.save();
+    res.status(201).json({success:true, data: newProduct});
+  } catch (error) {
+    console.error("Error in Create product:", error.message)
+    res.status(500).json({success: false, message: "Server Error"});
+  }
 })
 
 app.get("/heart", (req, res) => {
